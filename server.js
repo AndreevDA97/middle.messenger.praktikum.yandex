e96d1fb0283706aeebdda90fc9ef57f9fbb688a4
Andreev D.A <32336817+AndreevDA97@users.noreply.github.com>
2023-05-27 19:33:31 +0300

    Sprint 1 (#3)
    * Спринт 1
    
    * Поправки для развертывания проекта
    
    * Update README.md
    
    * Update package.json
    
    * Update package.json
    
    * Поправки по pull request

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+console.log(path.join(__dirname, "dist"));
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.listen(PORT, function () {
+  console.log(`App listening on port ${PORT}!`);
+});
